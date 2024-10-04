c6676b284bfdf6667927c4851ffb3e776468c96d
cryptic <bossguy2121@gmail.com>
2024-10-04 23:24:49 +0100

    Update number-converter.py

--- number-converter.py
+++ number-converter.py
@@ -1,16 +1,20 @@
-choice = input("Convert denary to binary (1)\nConvert binary to denary (2)\n")
+choice = int(input("Convert denary to binary (1)\nConvert binary to denary (2)\n"))
 if choice == 1:
     bitValue = 1
     bit = 1
     bitArray = [0] * 32
     intNum = int(input("Enter number: "))
-    while intNum != 0 and bitValue < intNum:
+    while intNum != 0:
         bitValue *= 2
         bit += 1
         if bitValue > intNum:
-            bitValue /= 2
+            bitValue //= 2
             bit -= 1
             if bit > 32:
                 print("Error, number is too large")
                 break
-        intNum -= bitValue
+            else:
+                bitArray[32 - bit] = 1
+                intNum -= bitValue
+    print("Binary:", end=" ")
+    print(*bitArray, sep="")
