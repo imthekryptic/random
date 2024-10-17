e344d933ac7b6e4b5e6392fbfd1e2ffcd587de0a
cryptic <bossguy2121@gmail.com>
2024-10-17 01:26:20 +0100

    Update convert-custom.py

--- converters/convert-custom.py
+++ converters/convert-custom.py
@@ -1,28 +1,50 @@
-def ConvertDenary(num):
-    try:
-        base = int(input("Enter base: "))
-    except ValueError and UnboundLocalError:
-        print("Error, invalid input\n")
-    if base != 2:
-        print(int(num, base))
-    else:
-        print("{0:b}".format(int(num)))
-        
-def ConvertBinary(num):
-    try:
-        base = int(input("Enter base: "))
-    except ValueError and UnboundLocalError:
-        print("Error, invalid input\n")
-    return print(int(num, base))
+def ConvertToDenary(num, base):
+    return print(int(num, base))  
+
+def ConvertToBinary(num):
+    return print(f"{int(num):b}")
+
+def ConvertToHex(num):
+    return print(f"{int(num):x}")
+
+def ConvertToOct(num):
+    return print(f"{int(num):o}")
+
+def ConvertCustom(num, base):
+    if not num:
+        return print(0)
+    digits = []
+    while num:
+        digits.append(num % base)
+        num //= base
+    digits = list(reversed(digits))
+    print(*digits, sep="")
 
 while True:
     try:
-        choice = int(input("Convert a denary number (1)\nConvert a binary number (2)\n"))
+        choice = int(input("Convert a number to denary (1)\nConvert a number to binary (2)\nConvert a number to hexadecimal (3)\nConvert a number to octal (4)\nConvert a number to a custom base (5)\n"))
         if choice == 1:
-            number = input("Enter number: ")
-            ConvertDenary(number)
+            try:
+                number = input("Enter number: ")
+                base = int(input("Enter its base: "))
+            except ValueError and UnboundLocalError:
+                print("Error, invalid input\n")
+            ConvertToDenary(number, base)
         if choice == 2:
             number = input("Enter number: ")
-            ConvertBinary(number)
+            ConvertToBinary(number)
+        if choice == 3:
+            number = input("Enter number: ")
+            ConvertToHex(number)
+        if choice == 4:
+            number = input("Enter number: ")
+            ConvertToOct(number)
+        if choice == 5:
+            try:
+                number = int(input("Enter number: "))
+                base = int(input("Enter its base: "))
+            except ValueError and UnboundLocalError:
+                print("Error, invalid input\n")
+            ConvertCustom(number, base)
     except ValueError or UnboundLocalError:
         print("Error, invalid input\n")
