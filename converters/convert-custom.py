d82b7234d970b32895daf31d664aa70d59a52a07
cryptic <bossguy2121@gmail.com>
2024-10-18 12:04:50 +0100

    Update convert-custom.py
    outputted values a-z are now printed in uppercase A-Z, code optimisations with try except

--- converters/convert-custom.py
+++ converters/convert-custom.py
@@ -5,7 +5,7 @@ def ConvertToBinary(num):
     return print(f"{int(num):b}")
 
 def ConvertToHex(num):
-    return print(f"{int(num):x}")
+    return print(f"{int(num):x}".upper())
 
 def ConvertToOct(num):
     return print(f"{int(num):o}")
@@ -72,17 +72,15 @@ def ConvertCustom(num, base):
             digits.append(num % base)
         num //= base
     digits = list(reversed(digits))
-    print(*digits, sep="")
+    string = "".join(str(digit) for digit in digits) # Converts each element to string
+    return print(string.upper())
 
 while True:
     try:
         choice = int(input("Convert a number to denary (1)\nConvert a number to binary (2)\nConvert a number to hexadecimal (3)\nConvert a number to octal (4)\nConvert a number to a custom base (5)\n"))
         if choice == 1:
-            try:
-                number = input("Enter number: ")
-                base = int(input("Enter its base: "))
-            except ValueError and UnboundLocalError:
-                print("Error, invalid input\n")
+            number = input("Enter number: ")
+            base = int(input("Enter its base: "))
             ConvertToDenary(number, base)
         if choice == 2:
             number = input("Enter number: ")
@@ -94,11 +92,8 @@ while True:
             number = input("Enter number: ")
             ConvertToOct(number)
         if choice == 5:
-            try:
-                number = int(input("Enter number: "))
-                base = int(input("Enter base to convert into: "))
-            except ValueError and UnboundLocalError:
-                print("Error, invalid input\n")
+            number = int(input("Enter number: "))
+            base = int(input("Enter base to convert into: "))
             ConvertCustom(number, base)
     except ValueError or UnboundLocalError:
         print("Error, invalid input\n")
