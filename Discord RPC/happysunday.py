16f7dad5daa46efc4faa6b075aac089215aa24f8
kryptic <bossguy2121@gmail.com>
2025-07-25 22:49:16 +0300

    Update day part of string to print "day" or "days" based on remaining time

--- Discord RPC/happysunday.py
+++ Discord RPC/happysunday.py
@@ -7,7 +7,7 @@ RPC = Presence(client_id)
 RPC.connect()
 
 while True:
-    days_ahead = 6 - datetime.datetime.now().weekday() # 6 is Sunday
+    days_ahead = 6 - datetime.datetime.now().weekday()  # 6 is Sunday
     if days_ahead < 0:
         days_ahead += 7
 
@@ -18,9 +18,15 @@ while True:
     datetime.UTC = datetime.datetime.utcfromtimestamp(next_sunday).timestamp()
     next_sunday_utc = int(next_sunday + (next_sunday - datetime.UTC))
     
+    if (int((next_sunday_utc - time.time()) / 86400)) > 1:
+        state=f"{int((next_sunday_utc - time.time()) / 86400)} days {int(((next_sunday_utc - time.time()) % 86400) / 3600):02}:{int(((next_sunday_utc - time.time()) % 3600) / 60):02}:{int((next_sunday_utc - time.time()) % 60):02}"
+    else:
+        state=f"{int((next_sunday_utc - time.time()) / 86400)} day {int(((next_sunday_utc - time.time()) % 86400) / 3600):02}:{int(((next_sunday_utc - time.time()) % 3600) / 60):02}:{int((next_sunday_utc - time.time()) % 60):02}"
+
+    
     RPC.update(
         details="Time until happy Sunday:",
-        state=f"{int((next_sunday_utc - time.time()) / 86400)} days {int(((next_sunday_utc - time.time()) % 86400) / 3600):02}:{int(((next_sunday_utc - time.time()) % 3600) / 60):02}:{int((next_sunday_utc - time.time()) % 60):02}",
+        state=state,
         large_image="happysunday",
         large_text="We wish you a happy Sunday",
         start=int(time.time()),
